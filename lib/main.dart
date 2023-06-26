d09a22070cd1c9a8183948119872b724fd58666c
Jeonkukil <a0211a@naver.com>
2023-06-27 00:02:16 +0900

    기기본 UI완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
+import 'package:actual1/common/component/custom_text_form_field.dart';
+import 'package:actual1/user/view/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(_App(),);
+  runApp(
+    _App(),
+  );
 }
 
 class _App extends StatelessWidget {
   const _App({Key? key}) : super(key: key);
+
   // 엑세스토큰, 리프레시 토큰활용예정(로그인)
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(),
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
       ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
     );
   }
 }
